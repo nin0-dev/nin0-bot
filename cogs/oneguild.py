fcad3838ca76e0cb119a58205b07c1f29fd0e014
nin0dev <support@nin0dev.com>
2023-12-31 12:47:43 -0500

    actually made oneguild work

--- cogs/oneguild.py
+++ cogs/oneguild.py
@@ -1,14 +1,19 @@
 # a skeleton cog you can use to make cogs
 import discord
 from discord.ext import commands
+import yaml
 
 class OneGuild(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
+    cfg = open('configuration.yaml', 'r')
+    config = yaml.safe_load(cfg)
+
     @commands.Cog.listener()
     async def on_guild_join(self, guild):
-        await guild.leave()
+        if config["guild_id"] == guild.id:
+            await guild.leave()
 
         
 def setup(bot): 
